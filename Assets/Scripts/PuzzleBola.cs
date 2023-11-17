c0f8e2051d545eb11b1b6b5251c45cfb1ed71d19
AnaMurillo <ana.murillo@alumnos.upm.es>
2023-11-17 16:49:27 +0100

    Script bola con recompensa

--- Assets/Scripts/PuzzleBola.cs
+++ Assets/Scripts/PuzzleBola.cs
@@ -6,12 +6,15 @@ public class PuzleBola : MonoBehaviour
 {
     [SerializeField] private TMP_Text textoGanador;
 
+    public Item itemRecompensa; // Asigna el objeto de recompensa 
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Esmeralda"))
         {
             Debug.Log("Esmeralda en hueco");
             textoGanador.gameObject.SetActive(true);
+            itemRecompensa.inventory.AddItem(itemRecompensa);
 
             // TODO Volver a la escena principal
         }
