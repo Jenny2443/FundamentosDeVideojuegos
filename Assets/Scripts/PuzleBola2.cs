5510318507635129f3129c607d80a9a6b228f2fc
Víctor Ramírez Castaño <vicdan0603@gmail.com>
2023-11-21 19:06:48 +0100

    Error de build 1 solucionado

--- Assets/Scripts/PuzleBola2.cs
+++ Assets/Scripts/PuzleBola2.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEngine.SceneManagement;
 
 public class PuzleBola2 : MonoBehaviour
