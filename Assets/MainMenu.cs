030bda0f959d0556bd91c0234352b85e4fd7600c
Víctor Ramírez Castaño <vicdan0603@gmail.com>
2024-01-06 10:42:30 +0100

    Cambio redireccion main menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -19,7 +19,7 @@ public class MainMenu : MonoBehaviour
     }
 
     public void EscenaJuego(){
-	    SceneManager.LoadScene("Escenario Pruebas");
+	    SceneManager.LoadScene("Andres");
     }
 
     public void EscenaOpciones(){
